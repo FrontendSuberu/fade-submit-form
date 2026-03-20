4cf51f3ea3f6016a57d64d1ffeed3f03f4c17817
FrontendSuberu <joeysuberu@gmail.com>
2026-03-20 23:48:57 +0100

    updated logic, added success text when form submitted

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const submitBtn = document.querySelector("#submitBtn");
 let invalidAttempts = 0;
 let formFilled = true;
 
+// validation check
 const updateSubmitState = () => {
   inputs.forEach((input) => {
     const warning = input.parentElement.querySelector("#warningBlock");
@@ -18,47 +19,56 @@ const updateSubmitState = () => {
     } else {
       warning.textContent = "";
     }
-
-    // input.addEventListener("input", () => {
-    //   updateSubmitState();
-    // });
   });
 
   const allValid = Array.from(inputs).every((input) => input.checkValidity());
   if (allValid) {
+    formFilled = true;
     submitBtn.disabled = false;
     submitBtn.textContent = "submit";
-    submitBtn.style.cssText = "opacity: 1; cursor: pointer;";
+    submitBtn.classList.remove("hidden");
+    submitBtn.style.cssText = "cursor: pointer;";
+    // submitBtn.classList.add("holdExcessive");
   }
 };
 
+// live validation block on each input
 inputs.forEach((input) => input.addEventListener("input", updateSubmitState));
 
+// prevent default behaviour, interaction check + implementation
 const handleFormUpdate = (e) => {
   e.preventDefault();
 
   updateSubmitState();
 
   if (formFilled) {
+    const submitStatus = document.querySelector("#form-status");
+    submitStatus.textContent = "Form submitted successfully";
+    setTimeout(() => {
+      submitStatus.textContent = "";
+    }, 1500);
     console.log("Form submitted successfully");
     form.reset();
     invalidAttempts = 0;
-    updateSubmitState();
+    // updateSubmitState();
   } else {
     invalidAttempts++;
   }
 
-  if (invalidAttempts >= 4) {
+  if (invalidAttempts >= 3) {
     formFilled = false;
     submitBtn.disabled = true;
     submitBtn.textContent = "removing...";
-    setTimeout(() => {
-      submitBtn.style.cssText = "opacity: 0; cursor: default;";
-    }, 800);
     submitBtn.classList.add("holdExcessive");
+    setTimeout(() => {
+      submitBtn.style.cssText = " cursor: default;";
+      submitBtn.classList.remove("holdExcessive");
+      submitBtn.classList.add("hidden");
+    }, 800);
   } else {
     formFilled = true;
-    submitBtn.classList.remove("holdExcessive");
+    submitBtn.disabled = false;
+    submitBtn.textContent = "submit";
   }
 };
 
